6f3722d09976338e09e8b1528c8dd83fe5f3befc
SrijeshBista <srijeshbista367@gmail.com>
2025-10-05 13:53:49 +0545

    srijeshbista

--- src/app/component/Nav.js
+++ src/app/component/Nav.js
@@ -1,15 +1,36 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Nav = () => {
+  const [hamopen, sethamopen] = useState(false);
+  {
+    /* function to open the hamburger popup */
+  }
+  const hamburgeropen = () => {
+    sethamopen(true)
+  };
   return (
-    <header>
+    <>
+      <header>
         <nav>
-            <div className='container'>
-                fegkjrb
+          <div className="container">
+            <div className="nav-main">
+              <div className="logo">
+                <img src="../Srijeshbistalogo.png" />
+              </div>
+              <div className="nav-btn">
+                <button>Get In Touch</button>
+              </div>
+              <div className="hamburger">
+                <GiHamburgerMenu />
+              </div>
             </div>
+          </div>
         </nav>
-    </header>
-  )
-}
+      </header>
+    </>
+  );
+};
 
 export default Nav;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './component/Nav'
 import './scss/style.scss';
+import Banner from './component/Banner';
 const page = () => {
   return (
     <div>
       <Nav/>
+      <Banner/>
     </div>
   )
 }
