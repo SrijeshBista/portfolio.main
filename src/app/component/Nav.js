9b07d2a5edb07c30c173633914daad5592cfe621
SrijeshBista <srijeshbista367@gmail.com>
2025-10-05 12:31:17 +0545

    srijesh

---
+++ src/app/component/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header>
+        <nav>
+            <div className='container'>
+                fegkjrb
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav;
