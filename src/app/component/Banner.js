b206976b31c1dc4a2d3d941400e61a323236fa22
SrijeshBista <srijeshbista367@gmail.com>
2025-10-06 15:52:56 +0545

    last push of the day

--- src/app/component/Banner.js
+++ src/app/component/Banner.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-    <section id='banner'>
-      <div className='container'>
-        <div className='padding'>
-            <div className='banner-main'>
-
+    <>
+      <section id="banner">
+        <div className="nav-container">
+          <div className="banner-main">
+            {/* left side of banner  */}
+            <div className="banner-left">
+              <div className="Name"><h1>SRIJESH BISTA</h1></div>
+              <hr/>
+              <div className="Profession"><h1>Front-End Developer</h1></div>
+              <hr/>
+              <div className=""></div>
             </div>
+            {/* right side of banner  */}
+            <div className="banner-right">
+              <img src="../banner.png" />
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
-  )
-}
+      </section>
+    </>
+  );
+};
 
 export default Banner;
