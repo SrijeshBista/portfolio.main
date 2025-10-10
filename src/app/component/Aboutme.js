aed38b992c40324d25fa4e10cf25df160210e186
SrijeshBista <srijeshbista367@gmail.com>
2025-10-10 16:50:46 +0545

    srijeshbista

--- src/app/component/Aboutme.js
+++ src/app/component/Aboutme.js
@@ -7,7 +7,7 @@ const Aboutme = () => {
             <div className='about-main'>
                 <div className='left-side'>
                         <h1>ABOUT ME</h1>
-                        <p>I'm Srijesh Bista, a Frontend Developer with a strong passion for crafting clean, responsive, and user-focused web experiences. I specialize in turning creative ideas into interactive digital solutions using technologies like React, Next.js, JavaScript, HTML, CSS, and Bootstrap.</p>
+                        <p>I&apos;m Srijesh Bista, a Frontend Developer with a strong passion for crafting clean, responsive, and user-focused web experiences. I specialize in turning creative ideas into interactive digital solutions using technologies like React, Next.js, JavaScript, HTML, CSS, and Bootstrap.</p>
                 </div>
                 <div className='right-side'>
 
