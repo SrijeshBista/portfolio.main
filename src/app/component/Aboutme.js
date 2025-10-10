16bbb27f498f4e60e69d8e98a1b2cf0608d61f24
SrijeshBista <srijeshbista367@gmail.com>
2025-10-10 17:01:27 +0545

    srijeshbista

--- src/app/component/Aboutme.js
+++ src/app/component/Aboutme.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Aboutme = () => {
   return (
-    <section id='aboutme'>
-      <div className='nav-container'>
-            <div className='about-main'>
-                <div className='left-side'>
-                        <h1>ABOUT ME</h1>
-                        <p>I&apos;m Srijesh Bista &lsquo;a Frontend Developer with a strong passion for crafting clean&lsquo; responsive&lsquo; and user-focused web experiences. I specialize in turning creative ideas into interactive digital solutions using technologies like React&lsquo; Next.js&lsquo; JavaScript&lsquo; HTML&lsquo; CSS&#39; and Bootstrap.</p>
-                </div>
-                <div className='right-side'>
-
-                </div>
-            </div>
-        
+    <section id="aboutme">
+      <div className="nav-container">
+        <div className="about-main">
+          <div className="left-side">
+            <h1>ABOUT ME</h1>
+            I&apos;m Srijesh Bista, a Frontend Developer with a strong passion
+            for crafting clean, responsive, and user-focused web experiences. I
+            specialize in turning creative ideas into interactive digital
+            solutions using technologies like React, Next.js, JavaScript, HTML,
+            CSS, and Bootstrap.
+          </div>
+          <div className="right-side"></div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Aboutme;
