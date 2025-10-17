3b0627f6515ad777bf7b103d63a74dc559a2728a
SrijeshBista <srijeshbista367@gmail.com>
2025-10-17 13:25:59 +0545

    srijehs bista

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,9 @@
 import { Inter, Orbitron } from "next/font/google";
 import "./globals.css";
-import Nav from "./component/Nav";
-import Footer from "./component/Footer";
 import ClientLayout from "./clintelayout";
+import Nav from "./component/Nav";
+import Footer from './component/Footer';
 import './scss/style.scss';
-import Head from "next/head";
 
 const interFont = Inter({
   variable: "--font-primary",
@@ -24,15 +23,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </Head>
       <body className={`${interFont.variable} ${orbitronFont.variable}`}>
         <Nav />
         <ClientLayout>{children}</ClientLayout>
-        <Footer />
+        <Footer/>
       </body>
     </html>
   );
 }
+  
